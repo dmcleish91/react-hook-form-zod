2db7d94c2a4ee852933ef5907977b78dee8350c1
Dwight McLeish Jr <dmcleishjr@gmail.com>
2022-12-12 22:23:29 -0500

    chore: inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Form from './components/Form';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className='form-container'>
+			<Form />
+		</div>
+	);
+}
+
+export default App;
